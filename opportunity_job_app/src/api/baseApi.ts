0725bc82744e70d94ca1bab351fbd6e2ec162ff8
Miloš Roknić <roknic.milos.994@gmail.com>
2023-10-14 22:47:09 +0200

    Implement Login page

--- opportunity_job_app/src/api/baseApi.ts
+++ opportunity_job_app/src/api/baseApi.ts
@@ -1,6 +1,5 @@
 "use client"
 import axios from "axios";
-// import { ENV } from "config/env";
 import { AuthService } from "./authService";
 
 const userData = {
@@ -8,34 +7,34 @@ const userData = {
     id: 1,
     accountType: "User"
 }
-    
-// const api = axios.create({
-// 	baseURL: ENV.BASE_API_URL,
-// });
+
+const api = axios.create({
+    baseURL: "http://localhost:8000/api", // TODO: get from env vars
+});
 
 export function setLocalStorage() {
     localStorage.setItem("testUser", JSON.stringify(userData));
 }
 
 const API = {
-	// getAllResources: (resource, queryParams = "") => {
-	// 	return api.get(`${resource}/?${queryParams}`);
-	// },
-	// getResourceById: (resource, resourceId) => {
-	// 	return api.get(`${resource}/${resourceId}`);
-	// },
-	// getProtectedResourceById: (resource, resourceId) => {
-	// 	const config = { headers: AuthService.getAuthorizationHeaders() };
-	// 	return api.get(`${resource}/${resourceId}`, config);
-	// },
-	// post: (url, data, config) => {
-	// 	return api.post(url, data, config);
-	// },
-	// patch: (url, data) => {
-	// 	const config = {};
-	// 	config.headers = AuthService.getAuthorizationHeaders();
-	// 	return api.patch(url, data, config);
-	// },
+    getAllResources: (resource, queryParams = "") => {
+        return api.get(`${ resource }/?${ queryParams }`);
+    },
+    getResourceById: (resource, resourceId) => {
+        return api.get(`${ resource }/${ resourceId }`);
+    },
+    getProtectedResourceById: (resource, resourceId) => {
+        const config = { headers: AuthService.getAuthorizationHeaders() };
+        return api.get(`${ resource }/${ resourceId }`, config);
+    },
+    post: (url, data, config) => {
+        return api.post(url, data, config);
+    },
+    patch: (url, data) => {
+        const config = {};
+        config.headers = AuthService.getAuthorizationHeaders();
+        return api.patch(url, data, config);
+    },
 };
 
 export default API;
---
+++ opportunity_job_app/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import "./login-form.scss"
+import InputField from "@/components/InputField/InputField";
+import { useState } from "react";
+import { AuthService } from "@/api/authService";
+import { useRouter } from "next/navigation"
+
+const LoginForm = () => {
+    const router = useRouter()
+    const [responseError, setResponseError] = useState<object | null>(null);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    async function login(e) {
+        e.preventDefault()
+        try {
+            await AuthService.login(formData.email, formData.password);
+            router.push("/")
+        } catch (error) {
+            setResponseError(error.response?.data?.errors?.non_field_errors);
+        }
+    }
+
+    const updateFormData = (fieldValue, fieldName) => {
+        setFormData({ ...formData, [fieldName]: fieldValue });
+    };
+
+    return (
+        <div className="wrapper">
+            <p className="welcome-sentence">Dobrodošli naza!</p>
+            <p className="welcome-sentence">Ulogujte se da biste nastavili.</p>
+            <form className="login-form">
+                <InputField
+                    label="E-mail adresa:"
+                    placeholder="Vaša e-mail adresa"
+                    onChange={ (value) => updateFormData(value, "email") }
+                />
+                <InputField
+                    label="Lozinka:"
+                    placeholder="Vaša lozinka"
+                    type="password"
+                    onChange={ (value) => updateFormData(value, "password") }
+                />
+                { responseError && <p className="error-message">{ responseError }</p> }
+                <button className="login-form__button" onClick={ login }>Uloguj se</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
