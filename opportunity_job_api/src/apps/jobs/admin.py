f26c9398503e5c11577f82563355d77f2c3d23c2
Damjan Vincic <damjanvincic2003@gmail.com>
2023-10-14 16:35:43 +0200

    Admin panel for FAQ

--- opportunity_job_api/src/apps/jobs/admin.py
+++ opportunity_job_api/src/apps/jobs/admin.py
@@ -1,3 +1,59 @@
+from django import forms
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from apps.common.admin import ModelAdmin
+from apps.jobs.models import FAQ
+
+class FAQForm(forms.ModelForm):
+    # question = forms.CharField()
+    # answer = forms.CharField()
+    # display_to_anonymous = forms.BooleanField()
+    # display_to_employers = forms.BooleanField()
+    # display_to_applicants = forms.BooleanField()
+    class Meta:
+        model = FAQ
+        fields = [
+            "question",
+            "answer",
+            "display_to_anonymous",
+            "display_to_employers",
+            "display_to_applicants",
+        ]
+
+
+@admin.register(FAQ)
+class FAQAdmin(ModelAdmin):
+    list_display = (
+        "question",
+        "answer",
+        "display_to_anonymous",
+        "display_to_employers",
+        "display_to_applicants",
+    )
+
+    search_fields = (
+        "question",
+        "answer",
+        "display_to_anonymous",
+        "display_to_employers",
+        "display_to_applicants",
+    )
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                "question",
+                "answer",
+                "display_to_anonymous",
+                "display_to_employers",
+                "display_to_applicants",
+            )
+        }),
+    )
+
+    # fieldsets= (
+    #     ("Question",  {"fields": "question"}),
+    # )
+
+# admin.site.register(FAQAdmin, FAQForm)
