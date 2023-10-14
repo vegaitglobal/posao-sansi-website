a6fc03acec397221acbba0fb4e560fc640c21bc1
Nemanja <slepimis120@gmail.com>
2023-10-14 10:52:04 +0200

    feat: implemented Swagger

--- opportunity_job_api/src/opportunity_job_api/urls.py
+++ opportunity_job_api/src/opportunity_job_api/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from opportunity_job_api.views import index_api_view
 
 urlpatterns = i18n_patterns(
@@ -16,6 +17,10 @@ api_urlpatterns = [
 
 urlpatterns += [
     path("api/", include((api_urlpatterns, "opportunity_job"), namespace="api")),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
