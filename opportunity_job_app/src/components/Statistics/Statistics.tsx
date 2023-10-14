781fb7b79bb6737b484cfdaebaf66d5f53c41d36
nikola.kokic <nikola.kokic@vegait.rs>
2023-10-14 21:34:37 +0200

    Update statistics

--- opportunity_job_app/src/components/Statistics/Statistics.tsx
+++ opportunity_job_app/src/components/Statistics/Statistics.tsx
@@ -1,42 +1,32 @@
 import "./Statistics.scss";
 
-type Employees = {
- number: number;
- text: string;
+type Employee = {
+  number: number;
+  text: string;
+  type: "employee" | "employer";
 };
 
-type Employeers = {
- number: number;
- text: string;
-};
-
-const employees: Employees[] = [
- { number: 23, text: "potražioci posla" },
+const statisticsData: Employee[] = [
+  { number: 23, text: "potražioci posla", type: "employee" },
+  { number: 45, text: "poslodavaca", type: "employer" },
 ];
 
-const employeers: Employeers[] = [{ number: 45, text: "poslodavaca" }];
-
 const Statistics = () => {
- return (
+  return (
     <div className="statistics">
-        <div className="statistics__container">
-            <h3 className="statistics__title">KORISNICI NAŠE APLIKACIJE</h3>
-            <div className="statistics__holder">
-                {employees.map((employee, index) => (
-                <div key={index}  className="statistics__box">
-                    <span className="statistics__box-number">{employee.number}</span>
-                    <span className="statistics__box-text p">{employee.text}</span>
-                </div>
-                 ))}
-                 {employeers.map((employer, index) => (
-                <div key={index} className="statistics__box">
-                    <span className="statistics__box-number">{employer.number}</span>
-                    <span className="statistics__box-text p">{employer.text}</span>
-                </div>
-                ))}
+      <div className="statistics__container">
+        <h3 className="statistics__title">KORISNICI NAŠE APLIKACIJE</h3>
+        <div className="statistics__holder">
+          {statisticsData.map((data, index) => (
+            <div key={index} className="statistics__box">
+              <span className="statistics__box-number">{data.number}</span>
+              <span className="statistics__box-text p">{data.text}</span>
             </div>
+          ))}
         </div>
+      </div>
     </div>
- );
-}
+  );
+};
+
 export default Statistics;
