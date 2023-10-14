01bcd59aa9c56d83ba03d03e2d62d06ce8953b66
jovan.bodroza <jovan.bodroza@vegait.rs>
2023-10-14 11:39:18 -0700

    refactor(jobs app): Code refactored

--- opportunity_job_api/src/apps/jobs/models/job_enrollment.py
+++ opportunity_job_api/src/apps/jobs/models/job_enrollment.py
@@ -2,8 +2,6 @@ from django.db import models
 from apps.common.models import BaseModel
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from apps.jobs.models import JobOffer
-from apps.users.models.applicant_account import ApplicantAccount
 
 class JobEnrollment(BaseModel):
 
@@ -11,5 +9,15 @@ class JobEnrollment(BaseModel):
         verbose_name = _("Job Enrollment")
         verbose_name_plural = _("Job Enrollments")
 
-    job_offer = models.ForeignKey(JobOffer, null=True, on_delete=models.CASCADE)
-    applicant_account = models.ForeignKey(ApplicantAccount, null=True, on_delete=models.CASCADE)
+    job_offer = models.ForeignKey(
+        verbose_name=_("job offer"),
+        to="jobs.JobOffer",
+        null=True,
+        on_delete=models.CASCADE
+    )
+    applicant_account = models.ForeignKey(
+        verbose_name=_("applicant account"),
+        to="users.ApplicantAccount",
+        null=True,
+        on_delete=models.CASCADE
+    )
--- opportunity_job_api/src/apps/jobs/models/job_offer.py
+++ opportunity_job_api/src/apps/jobs/models/job_offer.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from apps.jobs.enums import JobEngagement, JobCategory
 from apps.users.enums import Education, WorkExperience
-from apps.users.models.employer_account import EmployerAccount
 
 class JobOffer(BaseModel):
 
@@ -19,12 +18,19 @@ class JobOffer(BaseModel):
     job_description = models.TextField(
         verbose_name=_("job description"),
     )
-    employer = models.ForeignKey(EmployerAccount, null=True, on_delete=models.CASCADE)
+    employer = models.ForeignKey(
+        verbose_name=_("employer"),
+        to="users.EmployerAccount",
+        null=True,
+        on_delete=models.CASCADE
+    )
     location = models.CharField(
         verbose_name=_("location"),
         max_length=250,
     )
-    application_deadline = models.DateTimeField()
+    application_deadline = models.DateTimeField(
+        verbose_name=_("application deadline")
+    )
     engagement =  models.CharField(
         verbose_name=_("engagement"),
         max_length=20,
