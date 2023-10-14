d31c8bd99683363c6e5b5ef0917b2d8a96a583fa
Damjan Vincic <damjanvincic2003@gmail.com>
2023-10-14 18:20:08 +0200

    Add employer registration endpoint

--- opportunity_job_api/src/apps/users/serializers/__init__.py
+++ opportunity_job_api/src/apps/users/serializers/__init__.py
@@ -3,4 +3,7 @@ from apps.users.serializers.user_serializer import UserSerializer
 from apps.users.serializers.applicant_account_serializer import (
     ApplicantAccountSerializer,
 )
+from apps.users.serializers.employer_account_serializer import (
+    EmployerAccountSerializer
+)
 from apps.users.serializers.login_serializer import LoginSerializer
--- opportunity_job_api/src/apps/users/views/__init__.py
+++ opportunity_job_api/src/apps/users/views/__init__.py
@@ -1,2 +1,3 @@
-from apps.users.views.applicant_registration_api_view import RegisterApplicantAPIView
+from apps.users.views.applicant_registration_api_view import ApplicantRegistrationAPIView
 from apps.users.views.login_api_view import LoginAPIView
+from apps.users.views.employer_registration_api_view import EmployerRegistrationAPIView
