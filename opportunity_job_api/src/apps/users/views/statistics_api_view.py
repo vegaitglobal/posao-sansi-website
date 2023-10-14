cbae642b87fde1da92e51ee111563d4b8bbee113
Nemanja <slepimis120@gmail.com>
2023-10-14 21:49:15 +0200

    clean: removed unnecessary code from statistics_api_view.py

--- opportunity_job_api/src/apps/users/views/statistics_api_view.py
+++ opportunity_job_api/src/apps/users/views/statistics_api_view.py
@@ -6,7 +6,7 @@ from apps.users.utils import get_applicant_count, get_employer_count
 
 class StatisticsAPIView(APIView):
     @staticmethod
-    def get(request, **kwargs) -> JsonResponse:
+    def get(request) -> JsonResponse:
         applicant_count = get_applicant_count()
         employer_count = get_employer_count()
         return JsonResponse(data={"applicant_count": applicant_count, "employer_count": employer_count})
