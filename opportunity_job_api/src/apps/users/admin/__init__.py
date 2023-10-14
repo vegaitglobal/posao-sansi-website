70e1c5ddfda201cef4cd766dca03073a4c0bd855
Miloš Roknić <roknic.milos.994@gmail.com>
2023-10-14 16:03:41 +0200

    Refactor users app admin models

--- opportunity_job_api/src/apps/users/admin/__init__.py
+++ opportunity_job_api/src/apps/users/admin/__init__.py
@@ -1,3 +1,3 @@
-from apps.users.admin.user import UserAdmin
-from apps.users.admin.applicant_account import ApplicantAccount
-from apps.users.admin.employer_account import EmployerAccount
+from apps.users.admin.user_admin import UserAdmin
+from apps.users.admin.applicant_account_admin import ApplicantAccountAdmin
+from apps.users.admin.employer_account_admin import EmployerAccountAdmin
