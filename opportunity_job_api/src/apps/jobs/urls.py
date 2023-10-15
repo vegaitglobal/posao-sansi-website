c14089fbd4bff2d987dec3f394d216ac01395f9c
m-lukic <m.lukic@vegait.rs>
2023-10-15 02:52:54 +0200

    Update job offers endpoint; Add filters

--- opportunity_job_api/src/apps/jobs/urls.py
+++ opportunity_job_api/src/apps/jobs/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from apps.jobs.views import FAQListView
-from apps.jobs.views import JobOfferListAPIView
+from apps.jobs.views import JobOfferListCreateAPIView
 from apps.jobs.views import JobOfferDetailsAPIView
 from apps.jobs.views import JobEnrollmentListCreateAPIView
 
 urlpatterns = [
-    path("job-offers/", JobOfferListAPIView.as_view()),
+    path("job-offers/", JobOfferListCreateAPIView.as_view()),
     path("job-offers/<int:id>/", JobOfferDetailsAPIView.as_view()),
     path("job-enrollments/", JobEnrollmentListCreateAPIView.as_view(), name="job-enrollment"),
     path("faq/", FAQListView.as_view(), name="faq"),
--- opportunity_job_api/src/apps/jobs/views/job_offer_list_api_view.py
+++ opportunity_job_api/src/apps/jobs/views/job_offer_list_api_view.py
@@ -1,23 +1,12 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 from apps.jobs.models import JobOffer
 from apps.jobs.serializers import JobOfferSerializer
 
 
-class JobOfferListAPIView(APIView):
-
-    @staticmethod
-    def get(request, **kwargs):
-        job_offers = JobOffer.objects.all()
-        serializer = JobOfferSerializer(job_offers, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @staticmethod
-    def post(request, **kwargs):
-        serializer = JobOfferSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+class JobOfferListCreateAPIView(generics.ListCreateAPIView):
+    queryset = JobOffer.objects.all()
+    serializer_class = JobOfferSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['is_active', 'employer']
