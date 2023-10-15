94563e8ec64c578381e17232df6e45e9fc336656
Damjan Vincic <damjanvincic2003@gmail.com>
2023-10-15 16:55:00 +0200

    Protest POST job enrollment endpoint

--- opportunity_job_api/src/apps/jobs/views/job_enrollment_list_create_api_view.py
+++ opportunity_job_api/src/apps/jobs/views/job_enrollment_list_create_api_view.py
@@ -2,6 +2,10 @@ from apps.common.views import ListCreateAPIView
 from apps.jobs.filtersets import JobEnrollmentFilterSet
 from apps.jobs.models import JobEnrollment
 from apps.jobs.serializers import JobEnrollmentSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from apps.users.models import ApplicantAccount
 
 
 class JobEnrollmentListCreateAPIView(ListCreateAPIView):
@@ -9,3 +13,10 @@ class JobEnrollmentListCreateAPIView(ListCreateAPIView):
     serializer_class = JobEnrollmentSerializer
     filterset_class = JobEnrollmentFilterSet
     model_class = JobEnrollment
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        user_account = request.user.get_account()
+        if user_account and isinstance(user_account, ApplicantAccount):
+            return super(JobEnrollmentListCreateAPIView, self).create(request, *args, **kwargs)
+        return Response(status=status.HTTP_403_FORBIDDEN)
