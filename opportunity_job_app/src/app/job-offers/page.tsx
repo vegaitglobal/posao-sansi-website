ca281749f05bd3637b4251321f3d91ce3aff745c
Miloš Roknić <roknic.milos.994@gmail.com>
2023-10-15 11:45:43 +0200

    Implement dynamic route for Job Offer Details page

--- opportunity_job_app/src/app/job-offers/page.tsx
+++ opportunity_job_app/src/app/job-offers/page.tsx
@@ -4,18 +4,15 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import { useEffect, useState } from "react";
 import { AuthService } from "@/api/authService";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { JobOffer } from "@/api/models/JobOffer";
 import { JobOfferService } from "@/api/jobOfferService";
 import JobOffers from "@/components/JobOffers/JobOffers";
 import { User } from "@/api/models/User";
-import JobOffersDetails from "@/components/JobOfferDetails/JobOfferDetails";
 
 export default function JobOffersPage() {
     const router = useRouter();
-    const searchParams = useSearchParams();
 
-    const [jobOfferID, setJobOfferID] = useState<number>();
     const [applicant, setApplicant] = useState<User>();
     const [jobOffers, setJobOffers] = useState<JobOffer[]>([]);
     const [pageNumber, setPageNumber] = useState<number>(0);
@@ -31,13 +28,7 @@ export default function JobOffersPage() {
                 router.push("/");
             } else {
                 setApplicant(user);
-                const id = parseInt(searchParams.get("id"));
-                console.log("id:", id);
-                if (id) {
-                    setJobOfferID(id);
-                } else {
-                    loadMoreJobOffers();
-                }
+                loadMoreJobOffers();
             }
         };
         fetchJobOffers();
@@ -56,16 +47,12 @@ export default function JobOffersPage() {
         <>
             <Header user={ applicant }/>
             <main>
-                { jobOfferID ? (
-                    <JobOffersDetails jobOfferID={ jobOfferID }/>
-                ) : (
-                    <JobOffers
-                        onLoadMore={ loadMoreJobOffers }
-                        jobOffers={ jobOffers }
-                        hasNextPage={ hasNextPage }
-                        totalJobOfferNumber={ totalJobOfferNumber }
-                    />
-                ) }
+                <JobOffers
+                    onLoadMore={ loadMoreJobOffers }
+                    jobOffers={ jobOffers }
+                    hasNextPage={ hasNextPage }
+                    totalJobOfferNumber={ totalJobOfferNumber }
+                />
             </main>
             <Footer/>
         </>
