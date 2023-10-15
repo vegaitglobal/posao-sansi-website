01f3953f91f84bf6b2f0f283de8c944c9a26379c
srdjanp-vega <srdjan.pajic@vegait.rs>
2023-10-15 16:56:16 +0200

    Finish popup component

--- opportunity_job_app/src/components/Popup/Popup.tsx
+++ opportunity_job_app/src/components/Popup/Popup.tsx
@@ -1,18 +1,44 @@
+'use client';
 import "./Popup.scss";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const Popup = () => {
+type PopupElemetDetails = {
+    popup_visibility: boolean;
+    paragraph_first_text: string;
+    paragraph_seccond_text: string;
+    paragraph_second_visibility: boolean;
+    link_visibility: boolean;
+    link_text: string;
+    link_url: string;
+}
+
+const Popup = (props:{elementsDetails:PopupElemetDetails}) => {
+    const {elementsDetails } = props;
+
+    const [disp, setDisp] = useState('');
+    const [paragraphVis, setParagraphVis] = useState('');
+    const [linkVis, setLinkVis] = useState('');
+
+    useEffect(() => { setElementsVis()},[elementsDetails])
+
+    const closePopup = (e:any) => {e.stopPropagation(); setDisp('')};
+    const setElementsVis = () => {
+            if(elementsDetails.popup_visibility) {setDisp('popup--active')}else { setDisp('')}
+            if(elementsDetails.paragraph_second_visibility) {setParagraphVis('')}else {setParagraphVis('popup__element-hide')}
+            if(elementsDetails.link_visibility) {setLinkVis('')} else {setLinkVis('popup__element-hide')}
+    }
+    
     return (
-      <div className="popup">
-        <div className="popup__overlay">
-            <div className="popup__holder">
-                <button className="popup__close">
+      <div className={`popup ${disp}`}>
+        <div className="popup__overlay" onClick={closePopup}>
+            <div className="popup__holder" onClick={(e)=> {e.stopPropagation()}}>
+                <button className="popup__close" onClick={closePopup}>
                     <img src="/images/close.svg" alt="Close icon" className="popup__img" />
                 </button>
-                <span className="popup__text">Vaša registracija je uspešno obavljena!</span>
-                <span className="popup__text">Uskoro će Vam se javiti neko iz udruženja <span className="popup__text-bold">ATINA</span>.</span>
-                <Link href='/' className="popup__link">Nazad na početnu</Link>
+                <span className="popup__text">{elementsDetails.paragraph_first_text}</span>
+                <span className={`popup__text ${paragraphVis}`}>{elementsDetails.paragraph_seccond_text}</span>
+                <Link href={elementsDetails.link_url} className={`popup__link ${linkVis}`}>{elementsDetails.link_text}</Link>
             </div>
         </div>
       </div>  
