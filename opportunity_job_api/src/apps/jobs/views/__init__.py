467046c6604dd93d94d67287e05afd6a916c9ae5
Damjan Vincic <damjanvincic2003@gmail.com>
2023-10-15 15:58:20 +0200

    Add DELETE endpoint for job enrollments

--- opportunity_job_api/src/apps/jobs/views/__init__.py
+++ opportunity_job_api/src/apps/jobs/views/__init__.py
@@ -1,6 +1,5 @@
 from apps.jobs.views.faq_list_api_view import FAQListView
-from apps.jobs.views.job_enrollment_list_create_api_view import (
-    JobEnrollmentListCreateAPIView,
-)
+from apps.jobs.views.job_enrollment_list_create_api_view import JobEnrollmentListCreateAPIView
+from apps.jobs.views.job_enrollment_details_api_view import JobEnrollmentDetailsAPIView
 from apps.jobs.views.job_offer_details_api_view import JobOfferDetailsAPIView
 from apps.jobs.views.job_offer_list_api_view import JobOfferListCreateAPIView
