fb8db136c3d196cd7521d0ed8a0d7a7b38c71ce8
Miloš Roknić <roknic.milos.994@gmail.com>
2023-10-16 19:34:03 +0200

    Add env vars for Next JS app

--- opportunity_job_app/next.config.js
+++ opportunity_job_app/next.config.js
@@ -1,4 +1,36 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
 
-module.exports = nextConfig
+const nextConfig = {
+    // Will be available on both server and client:
+    publicRuntimeConfig: {
+        baseUrl: process.env.BASE_URL,
+        baseApiUrl: process.env.API_URL,
+        staticFolder: process.env.STATIC_DIR, // Will be used per SEO page default:
+        baseSeo: {
+            robotsProps: {
+                maxSnippet: -1, maxImagePreview: "none", maxVideoPreview: -1,
+            },
+        },
+        name: "Posao Šansi",
+        title: "Posao Šansi",
+        slogan: "", // TODO
+        description: "", // TODO
+        author: "", // TODO
+        logo: "images/logo.png",
+        imageShare: "", // TODO
+        facebookUrl: "https://www.facebook.com/NGOAtina",
+        instagramUrl: "http://atina.org.rs/",
+        linkedinUrl: "https://rs.linkedin.com/in/ngo-atina-129948216",
+        twitterUrl: "https://twitter.com/atinango",
+        phone: "+381616384071", // TODO
+        address: "Novi Sad",
+        region: "Vojvodina",
+        country: "RS",
+        postalCode: "21000",
+        locale: "sr",
+    },
+};
+
+console.log("nextConfig:", nextConfig);
+
+module.exports = nextConfig;
