db40fcb4dc12a685ff896dadd0a822a422420bdd
nikola.kokic <nikola.kokic@vegait.rs>
2023-10-18 08:17:39 +0200

    Update code for PR

--- opportunity_job_app/src/components/Popup/Popup.tsx
+++ opportunity_job_app/src/components/Popup/Popup.tsx
@@ -1,32 +1,49 @@
-'use client';
+"use client";
 import "./Popup.scss";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 type PopupElemetDetails = {
-    popup_visibility: boolean;
-    paragraph_first_text: string;
-    paragraph_second_text: string;
-    paragraph_second_visibility: boolean;
-    link_visibility: boolean;
-    link_text: string;
-    link_url: string;
+    popupVisibility: boolean;
+    paragraphFirstText: string;
+    paragraphSecondText: string;
+    paragraphSecondVisibility: boolean;
+    linkVisibility: boolean;
+    linkText: string;
+    linkUrl: string;
 }
 
 const Popup = (props:{elementsDetails:PopupElemetDetails}) => {
     const {elementsDetails } = props;
 
-    const [disp, setDisp] = useState('');
-    const [paragraphVis, setParagraphVis] = useState('');
-    const [linkVis, setLinkVis] = useState('');
+    const [disp, setDisp] = useState("");
+    const [paragraphVis, setParagraphVis] = useState("");
+    const [linkVis, setLinkVis] = useState("");
 
-    useEffect(() => { setElementsVis()},[elementsDetails])
+    useEffect(() => { 
+        setElementsVis()
+    },[elementsDetails])
 
-    const closePopup = (e:any) => {e.stopPropagation(); setDisp('')};
+    const closePopup = (e:any) => {
+        e.stopPropagation(); setDisp("")
+    };
+
     const setElementsVis = () => {
-            if(elementsDetails.popup_visibility) {setDisp('popup--active')}else { setDisp('')}
-            if(elementsDetails.paragraph_second_visibility) {setParagraphVis('')}else {setParagraphVis('popup__element-hide')}
-            if(elementsDetails.link_visibility) {setLinkVis('')} else {setLinkVis('popup__element-hide')}
+        if(elementsDetails.popupVisibility) {
+            setDisp("popup--active")
+        }else { 
+            setDisp("")
+        }
+        if(elementsDetails.paragraphSecondVisibility) {
+            setParagraphVis("")
+        }else {
+            setParagraphVis("popup__element-hide")
+        }
+        if(elementsDetails.linkVisibility) {
+            setLinkVis("")
+        }else {
+            setLinkVis("popup__element-hide")
+        }
     }
     
     return (
@@ -36,9 +53,9 @@ const Popup = (props:{elementsDetails:PopupElemetDetails}) => {
                 <button className="popup__close" onClick={closePopup}>
                     <img src="/images/close.svg" alt="Close icon" className="popup__img" />
                 </button>
-                <span className="popup__text">{elementsDetails.paragraph_first_text}</span>
-                <span className={`popup__text ${paragraphVis}`}>{elementsDetails.paragraph_second_text}</span>
-                <Link href={elementsDetails.link_url} className={`popup__link ${linkVis}`}>{elementsDetails.link_text}</Link>
+                <span className="popup__text">{elementsDetails.paragraphFirstText}</span>
+                <span className={`popup__text ${paragraphVis}`}>{elementsDetails.paragraphSecondText}</span>
+                <Link href={elementsDetails.linkUrl} className={`popup__link ${linkVis}`}>{elementsDetails.linkText}</Link>
             </div>
         </div>
       </div>  
