655adc079f532ea145710944a2d7ac2b23e46fee
Milos Roknic <m.roknic@vegait.rs>
2023-10-24 21:02:41 +0200

    Move translations related code to dictionaries and add About component translations

--- opportunity_job_app/src/app/[lang]/dictionaries/Dictionary.ts
+++ opportunity_job_app/src/app/[lang]/dictionaries/Dictionary.ts
@@ -1,38 +1,29 @@
-import en from "./dictionaries/en.json";
-import sr from "./dictionaries/sr.json";
-import { SERBIAN_LOCALE } from "@/appData/locales";
-
-
 export interface Dictionary {
   homeBanner: {
     title: string
     text: string
-  },
+  };
   homeCardLinks: {
     anonymousApplicantCardLink: {
       title: string
       label: string
-    },
+    }
     anonymousEmployerCardLink: {
       title: string
       label: string
-    },
+    }
     applicantCardLink: {
       title: string
       label: string
-    },
+    }
     employerCardLink: {
       title: string
       label: string
     }
-  }
+  };
+  about: {
+    title: string
+    missionText: string
+    visionText: string
+  };
 }
-
-const dictionaries: any = {
-  en: en as Dictionary,
-  sr: sr as Dictionary,
-};
-
-export const getDictionary = (locale: string): Dictionary => {
-  return { ...dictionaries[SERBIAN_LOCALE], ...dictionaries[locale] };
-};
---
+++ opportunity_job_app/src/app/[lang]/dictionaries/index.ts
@@ -0,0 +1,14 @@
+import en from "./en.json";
+import sr from "./sr.json";
+import { SERBIAN_LOCALE } from "@/appData/locales";
+import { Dictionary } from "@/app/[lang]/dictionaries/Dictionary";
+
+
+const dictionaries: any = {
+  en: en as Dictionary,
+  sr: sr as Dictionary,
+};
+
+export const getDictionary = (locale: string): Dictionary => {
+  return { ...dictionaries[SERBIAN_LOCALE], ...dictionaries[locale] };
+};
