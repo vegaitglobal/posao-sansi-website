4399c76b535b0515a4dfaa4eb3cff3520332e381
Milos Roknic <m.roknic@vegait.rs>
2023-10-24 19:40:35 +0200

    Fix Popup in PasswordForgottenForm component

--- opportunity_job_app/src/components/PasswordForgottenForm/PasswordForgottenForm.tsx
+++ opportunity_job_app/src/components/PasswordForgottenForm/PasswordForgottenForm.tsx
@@ -8,61 +8,62 @@ import Popup from "@/components/Popup/Popup";
 
 
 const PasswordForgottenForm = () => {
-    const [hasAccess, setHasAccess] = useState<boolean>(false);
-    const [hasOpenedPopup, setHasOpenedPopup] = useState<boolean>(false);
-    const [formData, setFormData] = useState({
-        email: ""
-    });
+  const [ hasAccess, setHasAccess ] = useState<boolean>(false);
+  const [ hasOpenedPopup, setHasOpenedPopup ] = useState<boolean>(false);
+  const [ formData, setFormData ] = useState({
+    email: ""
+  });
 
-    useEffect(() => {
-        checkAccess();
-    }, []);
+  useEffect(() => {
+    checkAccess();
+  }, []);
 
-    const checkAccess = () => {
-        if (AuthService.isAuthenticated()) {
-            window.location.href = "/";
-        } else {
-            setHasAccess(true);
-        }
-    };
+  const checkAccess = () => {
+    if (AuthService.isAuthenticated()) {
+      window.location.href = "/";
+    } else {
+      setHasAccess(true);
+    }
+  };
 
-    const forgotPassword = async (e: SyntheticEvent<EventTarget>) => {
-        e.preventDefault();
-        try {
-            await AuthService.requestPasswordReset(formData.email);
-            setHasOpenedPopup(true);
-        } catch (error: any) {
-        }
-    };
+  const forgotPassword = async (e: SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
+    try {
+      await AuthService.requestPasswordReset(formData.email);
+      setHasOpenedPopup(true);
+    } catch (error: any) {
+    }
+  };
 
-    const updateFormData = (fieldValue: string, fieldName: string) => {
-        setFormData({ ...formData, [fieldName]: fieldValue });
-    };
+  const updateFormData = (fieldValue: string, fieldName: string) => {
+    setFormData({ ...formData, [fieldName]: fieldValue });
+  };
 
-    if (!hasAccess) return null;
+  if (!hasAccess) return null;
 
-    return (
-        <div className="wrapper">
-            <p className="welcome-sentence">
-                Unesite Vašu e-mail adresu kako bismo Vam poslali podatke za resetovanje lozinke.
-            </p>
-            <form className="password-forgotten-form">
-                <InputField
-                    label="E-mail adresa:"
-                    placeholder="Vaša e-mail adresa"
-                    onChange={ (value) => updateFormData(value, "email") }
-                />
-                <button className="password-forgotten-form__button" onClick={ forgotPassword }>
-                    ZAHTEVAJ LINK
-                </button>
-            </form>
-            <Popup
-                isOpened={ hasOpenedPopup }
-                primaryText="Ukoliko nalog sa ovom mejl adresom postoji, poslaćemo mejl za resetovanje lozinke."
-                linkButton={ { label: "Nazad na početnu", url: "/" } }
-            />
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <p className="welcome-sentence">
+        Unesite Vašu e-mail adresu kako bismo Vam poslali podatke za resetovanje lozinke.
+      </p>
+      <form className="password-forgotten-form">
+        <InputField
+          label="E-mail adresa:"
+          placeholder="Vaša e-mail adresa"
+          onChange={ (value) => updateFormData(value, "email") }
+        />
+        <button className="password-forgotten-form__button" onClick={ forgotPassword }>
+          ZAHTEVAJ LINK
+        </button>
+      </form>
+      <Popup
+        isOpened={ hasOpenedPopup }
+        onClose={ () => setHasOpenedPopup(false) }
+        primaryText="Ukoliko nalog sa ovom mejl adresom postoji, poslaćemo mejl za resetovanje lozinke."
+        linkButton={ { label: "Nazad na početnu", url: "/" } }
+      />
+    </div>
+  );
 };
 
 export default PasswordForgottenForm;
