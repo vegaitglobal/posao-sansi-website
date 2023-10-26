0f906e60b51ec67b8547c2f8a34540d751d4eb91
Milos Roknic <m.roknic@vegait.rs>
2023-10-26 11:36:33 +0200

    Refactor LinksDictionary

--- opportunity_job_app/src/app/[lang]/dictionaries/components/LinksDictionary.ts
+++ opportunity_job_app/src/app/[lang]/dictionaries/components/LinksDictionary.ts
@@ -1,16 +1,16 @@
 export interface LinksDictionary {
   logoText: string;
   languageMenu: {
-    srLabel: string;
-    enLabel: string;
+    "srLabel": string;
+    "enLabel": string;
   };
   mainMenu: {
-    homeLabel: string;
-    faqLabel: string;
-    logoutLabel: string;
-    loginLabel: string;
-    registerLabel: string;
-    myJobsLabel: string;
-    jobOffersLabel: string;
+    "homeLabel": string;
+    "faqLabel": string;
+    "logoutLabel": string;
+    "loginLabel": string;
+    "registerLabel": string;
+    "myJobsLabel": string;
+    "jobOffersLabel": string;
   };
 }
