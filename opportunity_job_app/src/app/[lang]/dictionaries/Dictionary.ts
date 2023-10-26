e8e4ccc598f11f18b884e260941cc4ef380ce125
Milos Roknic <m.roknic@vegait.rs>
2023-10-26 09:27:59 +0200

    Add serbian translations for Footer component

--- opportunity_job_app/src/app/[lang]/dictionaries/Dictionary.ts
+++ opportunity_job_app/src/app/[lang]/dictionaries/Dictionary.ts
@@ -3,6 +3,7 @@ import { HomeBannerDictionary } from "@/app/[lang]/dictionaries/components/HomeB
 import { HomeCardLinksDictionary } from "@/app/[lang]/dictionaries/components/HomeCardLinksDictionary";
 import { AboutDictionary } from "@/app/[lang]/dictionaries/components/AboutDictionary";
 import { StatisticsDictionary } from "@/app/[lang]/dictionaries/components/StatisticsDictionary";
+import { FooterDictionary } from "@/app/[lang]/dictionaries/components/FooterDictionary";
 
 export interface Dictionary {
   header: LinksDictionary;
@@ -10,4 +11,5 @@ export interface Dictionary {
   homeCardLinks: HomeCardLinksDictionary;
   about: AboutDictionary;
   statistics: StatisticsDictionary;
+  footer: FooterDictionary;
 }
---
+++ opportunity_job_app/src/app/[lang]/dictionaries/components/FooterDictionary.ts
@@ -0,0 +1,8 @@
+export interface FooterDictionary {
+  socialMediaListTitle: string;
+  contactListTitle: string;
+  termsOfUseFilename: string;
+  privacyPolicyFilename: string;
+  copyrightText: string;
+  designedByText: string;
+}
