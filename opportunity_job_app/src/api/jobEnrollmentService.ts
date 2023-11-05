fb2405bcaca654f9be157f1e9dc31b5a6b937925
Milos Roknic <roknic.milos.994@gmail.com>
2023-11-05 09:57:07 +0100

    Implement createJobOffer method in JobOfferService

--- opportunity_job_app/src/api/jobEnrollmentService.ts
+++ opportunity_job_app/src/api/jobEnrollmentService.ts
@@ -1,12 +1,12 @@
-"use client"
 import API from "./baseApi";
 
 export const JobEnrollmentService = {
-    addJobEnrollment: async (jobOfferId: number, applicant_account: number) => {
-        return await API.post(`/job-enrollments/`, { job_offer: jobOfferId, applicant_account:applicant_account });
-    },
+  addJobEnrollment: async (jobOfferId: number, applicant_account: number) => {
+    const data = { job_offer: jobOfferId, applicant_account: applicant_account };
+    return await API.post("/job-enrollments/", data);
+  },
 
-    removeJobEnrollment: async (jobEnrollmentId: number) => {
-        return await API.delete(`/job-enrollments/${jobEnrollmentId}/`);
-    }
-}
+  removeJobEnrollment: async (jobEnrollmentId: number) => {
+    return await API.delete(`/job-enrollments/${ jobEnrollmentId }/`);
+  }
+};
--- opportunity_job_app/src/api/jobOfferService.ts
+++ opportunity_job_app/src/api/jobOfferService.ts
@@ -1,45 +1,50 @@
 import API from "./baseApi";
 import { JobOffer, jobOfferFlags } from "@/api/models/JobOffer";
 import { Pagination } from "@/api/models/Pagination";
+import { JobOfferFormData } from "@/components/RegistrationForm/types";
 
 
 interface JobOfferListResponse {
-    items: JobOffer[],
-    pagination: Pagination
+  items: JobOffer[],
+  pagination: Pagination
 }
 
 export const JobOfferService = {
-    getActiveJobOffers: async (pageNumber: number): Promise<JobOfferListResponse> => {
-        const listResponse = await JobOfferService.getJobOffers(pageNumber);
-        listResponse.items = listResponse.items.map(jobOffer => {
-            return { ...jobOffer, flag: jobOffer.has_enrolled ? jobOfferFlags.enrolled : undefined };
-        });
-        return listResponse;
-    },
+  getActiveJobOffers: async (pageNumber: number): Promise<JobOfferListResponse> => {
+    const listResponse = await JobOfferService.getJobOffers(pageNumber);
+    listResponse.items = listResponse.items.map(jobOffer => {
+      return { ...jobOffer, flag: jobOffer.has_enrolled ? jobOfferFlags.enrolled : undefined };
+    });
+    return listResponse;
+  },
 
-    getMyJobsOffers: async (pageNumber: number): Promise<JobOfferListResponse> => {
-        const listResponse = await JobOfferService.getJobOffers(pageNumber);
-        listResponse.items = listResponse.items.map(jobOffer => {
-            return { ...jobOffer, flag: jobOffer.is_active ? jobOfferFlags.active : jobOfferFlags.archived };
-        });
-        return listResponse;
-    },
+  getMyJobsOffers: async (pageNumber: number): Promise<JobOfferListResponse> => {
+    const listResponse = await JobOfferService.getJobOffers(pageNumber);
+    listResponse.items = listResponse.items.map(jobOffer => {
+      return { ...jobOffer, flag: jobOffer.is_active ? jobOfferFlags.active : jobOfferFlags.archived };
+    });
+    return listResponse;
+  },
 
-    getJobOffers: async (pageNumber: number): Promise<JobOfferListResponse> => {
-        const params = new URLSearchParams({
-            ipp: 6,
-            page: pageNumber,
-        } as any);
-        const response = await API.getList("job-offers", params.toString());
-        return response.data;
-    },
+  getJobOffers: async (pageNumber: number): Promise<JobOfferListResponse> => {
+    const params = new URLSearchParams({
+      ipp: 6,
+      page: pageNumber,
+    } as any);
+    const response = await API.getList("job-offers", params.toString());
+    return response.data;
+  },
 
-    findJobOffer: async (jobOfferID: number): Promise<JobOffer> => {
-        const response = await API.getOne("job-offers", jobOfferID);
-        return response.data;
-    },
-    
-    toggleJobOffer: async (jobOfferId: number, updatedData: any) => {
-        return await API.patch(`/job-offers/${jobOfferId}/`, updatedData);
-    }
+  findJobOffer: async (jobOfferID: number): Promise<JobOffer> => {
+    const response = await API.getOne("job-offers", jobOfferID);
+    return response.data;
+  },
+
+  toggleJobOffer: async (jobOfferId: number, updatedData: any) => {
+    return await API.patch(`/job-offers/${ jobOfferId }/`, updatedData);
+  },
+
+  createJobOffer: async (jobOffer: JobOfferFormData) => {
+    return await API.post("/job-offers/", jobOffer);
+  }
 };
