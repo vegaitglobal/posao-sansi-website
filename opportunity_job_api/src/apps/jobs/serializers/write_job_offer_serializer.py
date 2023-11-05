51b9b9730875f7696ab1375c264f8c92ffc1731d
Milos Roknic <roknic.milos.994@gmail.com>
2023-11-05 09:53:40 +0100

    Add extra application_deadline field validation in WriteJobOfferSerializer

--- opportunity_job_api/src/apps/jobs/serializers/write_job_offer_serializer.py
+++ opportunity_job_api/src/apps/jobs/serializers/write_job_offer_serializer.py
@@ -1,3 +1,7 @@
+from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
+from rest_framework.exceptions import ValidationError
+
 from apps.common.serializers import ModelSerializer
 from apps.jobs.models import JobOffer
 
@@ -6,3 +10,11 @@ class WriteJobOfferSerializer(ModelSerializer):
     class Meta:
         model = JobOffer
         fields = "__all__"
+
+    def validate(self, attrs) -> dict:
+        validated_data = super().validate(attrs=attrs)
+        if validated_data["application_deadline"] < now():
+            raise ValidationError(
+                {"application_deadline": _("Application deadline must be in the future")}
+            )
+        return validated_data
